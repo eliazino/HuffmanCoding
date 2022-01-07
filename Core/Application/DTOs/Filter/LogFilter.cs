ad7efcd255b97729deb8e849f74e26a1372ab15b
eliazino <elias.akin@gmail.com>
2022-01-07 08:35:50 +0100

    filters

---
+++ Core/Application/DTOs/Filter/LogFilter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Application.DTOs.Filter {
+    public class LogFilter : Filter {
+        public long humanID {
+            get {
+                string thisName = GetCallerName();
+                return (long)getField(thisName);
+            }
+            set {
+                string thisName = GetCallerName();
+                if (fieldIsSet(thisName)) {
+                    reference[thisName] = value;
+                } else {
+                    reference.Add(thisName, value);
+                }
+            }
+        }
+        public long category {
+            get {
+                string thisName = GetCallerName();
+                return (long)getField(thisName);
+            }
+            set {
+                string thisName = GetCallerName();
+                if (fieldIsSet(thisName)) {
+                    reference[thisName] = value;
+                } else {
+                    reference.Add(thisName, value);
+                }
+            }
+        }
+        public long id {
+            get {
+                string thisName = GetCallerName();
+                return (long)getField(thisName);
+            }
+            set {
+                string thisName = GetCallerName();
+                if (fieldIsSet(thisName)) {
+                    reference[thisName] = value;
+                } else {
+                    reference.Add(thisName, value);
+                }
+            }
+        }
+    }
+}

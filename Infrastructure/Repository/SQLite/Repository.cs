41945c4572b352afd19b20d4e88c9787200813fb
eliazino <elias.akin@gmail.com>
2022-01-07 08:37:14 +0100

    Repository concrete

--- Infrastructure/Repository/SQLite/Repository.cs
+++ Infrastructure/Repository/SQLite/Repository.cs
@@ -2,11 +2,11 @@
 using Core.Application.DTOs.Local;
 using Core.Shared;
 using Infrastructure.Interfaces;
-using Infrastructure.Interfaces.MongoDB;
 using Infrastructure.Interfaces.SQlite;
 using NetCore.AutoRegisterDi;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,9 +22,55 @@ namespace Infrastructure.Repository.SQLite {
         public void beginTransacion() {
             IDBCommand.beginTransaction();
         }
-
+        public async Task<bool> executeInLine(string sql, IEnumerable<object> q) {
+            IDBCommand.prepare(sql);
+            IDBCommand.bindValues(q);
+            return await IDBCommand.execute();
+        }
         public async Task<bool> commit() {
             return await IDBCommand.commit();
+        }
+
+        private async Task<bool> tableExists(string tableName) {
+            string sql = "SELECT name FROM sqlite_master WHERE type='table' AND name = ?";
+            return await isExist(sql, new List<object> { tableName });
+        }
+
+        public async Task<bool> createTableIfNotExists<T>(T tableObject) {
+            string tableName = typeof(T).ToString().Split('.')[^1];
+            if (await tableExists(tableName))
+                return true;
+            List<string> columns = new List<string>();
+            Type propType = tableObject.GetType();
+            foreach (PropertyInfo info in propType.GetProperties()) {
+                string lineColumn = string.Empty;
+                if (info.Name == "id") {
+                    lineColumn = getLineCreate(info.Name, null, true, false);
+                    columns.Add(lineColumn);
+                    continue;
+                }
+                Type infoType = info.PropertyType;
+                string fieldType = "TEXT";
+                if (new List<Type> { typeof(int), typeof(double), typeof(float), typeof(long), typeof(decimal) }.Contains(infoType)) {
+                    fieldType = "NUMERIC";
+                }
+                lineColumn = getLineCreate(info.Name, fieldType, false, true);
+                columns.Add(lineColumn);
+                continue;
+            }
+            string sql = "CREATE TABLE " + tableName + " (" + string.Join(',', columns) + ")";
+            IDBCommand.beginTransaction();
+            IDBCommand.prepare(sql);
+            await IDBCommand.execute();
+            return await IDBCommand.commit();
+        }
+
+        private string getLineCreate(string columnName, string type, bool autoIncrement, bool nullable) {
+            type = autoIncrement ? "INTEGER" : type;
+            string key = autoIncrement ? "PRIMARY KEY" : "";
+            string nullableField = autoIncrement ? "" : nullable ? "NULL" : "NOT NULL";
+            string autoField = autoIncrement ? "AUTOINCREMENT" : "";
+            return string.Join(' ', new List<string> { columnName, type, key, autoField, nullableField });
         }
 
         public async Task<bool> isExist(string sql, IEnumerable<object> q) {
@@ -48,18 +94,13 @@ namespace Infrastructure.Repository.SQLite {
         private async Task<IEnumerable<Dictionary<string, object>>> selectFromQuery(string sql, List<object> q) {
             IDBCommand.prepare(sql);
             if (q != null) { IDBCommand.bindValues(q); }
+            await IDBCommand.execute();
             var data = await IDBCommand.fetchAllAsObj();
             return data;
         }
 
-        public async Task<bool> logActivity(string logDetails, string logCategory, string creator, string issuerID) {
-            IDBCommand.prepare("insert into activitylog (actionTime, actionDetails, actionCategory, username, issuerID) values (:actionTime, :actionDetails, :actionCategory, :username, :issuerID)");
-            IDBCommand.bindValue("@actionTime", Utilities.getTodayDate().unixTimestamp.ToString());
-            IDBCommand.bindValue("@actionDetails", logDetails);
-            IDBCommand.bindValue("@actionCategory", logCategory);
-            IDBCommand.bindValue("@username", creator);
-            IDBCommand.bindValue("@issuerID", issuerID);
-            return await IDBCommand.execute();
+        protected class Counter {
+            public int totalCount { get; set; }
         }
     }
 }
